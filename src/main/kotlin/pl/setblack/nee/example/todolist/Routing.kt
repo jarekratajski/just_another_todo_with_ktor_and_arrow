3c50a149558bf515b8901787a1d3cb5664cdfc9e
jarekratajski <jratajski@gmail.com>
2021-03-29 09:29:03 +0200

    hello world

--- src/main/kotlin/pl/setblack/nee/example/todolist/Routing.kt
+++ src/main/kotlin/pl/setblack/nee/example/todolist/Routing.kt
@@ -1,5 +1,30 @@
 package pl.setblack.nee.example.todolist
 
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun Route.customerRouting() {
+    route("/hello") {
+        get {
+            call.respond("hello world")
+        }
+    }
+}
+
 fun main() = run {
-    println("started")
+    embeddedServer(Netty, port = 8000) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
+    Unit
 }
