29980193c49199d2240b9b82209c45f8136d7754
jarekratajski <jratajski@gmail.com>
2021-03-29 20:26:04 +0200

    hello server

---
+++ src/main/kotlin/pl/setblack/nee/example/todolist/HelloServer.kt
@@ -0,0 +1,33 @@
+package pl.setblack.nee.example.todolist
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+@Suppress("ReturnUnit")
+object HelloServer {
+
+    fun startServer() =
+        embeddedServer(Netty, port = 8000) {
+            helloRouting()
+        }.start(wait = true)
+
+    fun Application.helloRouting() {
+        routing {
+            route("/hello") {
+                get {
+                    call.respond("hello world")
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    HelloServer.startServer()
+}
